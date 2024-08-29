20729a20a6646b16ba5405a6b8d4e2cd8c7665eb
Shubhro Banerjee <shubhro.banerjee@netwoven.com>
2024-08-29 15:05:21 +0530

    fix: msal login new app

--- azureAD_react_POC/src/auth-config.ts
+++ azureAD_react_POC/src/auth-config.ts
@@ -1,6 +1,6 @@
 export const msalConfig = {
 	auth: {
-		clientId: "09f8a17f-5f64-4367-b50c-53f6cd6fafcb",
+		clientId: "a7d78880-3cd0-46aa-8193-e5168f89aa32",
 		// authority: "https://login.microsoftonline.com/common/", // For multi-tenant applications
 		authority: "https://login.microsoftonline.com/shubhroPOC.onmicrosoft.com", // For single-tenant applications
 		redirectUri: "http://localhost:5173", // Replace with your actual redirect URI
